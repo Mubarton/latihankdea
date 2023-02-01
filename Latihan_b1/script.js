5a7d6f3207a35b21823d3939690425a9fb15bab7
Mutiara Akbar Nst <mutiaraakbarnst03@gmail.com>
2023-02-02 01:36:14 +0700

    Bintang 1 Selesai

---
+++ Latihan_b1/script.js
@@ -0,0 +1,5 @@
+const ubah_warna = () => {
+    var backgr = document.getElementById("backgr");
+    var warna = document.getElementById("masukan").value;
+    backgr.style.backgroundColor = warna;
+}
